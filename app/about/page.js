2d6e432ab897e95eba29b4fa4a526cf776e778e4
chai-py <er.chaital123@gmail.com>
2024-11-26 14:38:12 -0700

    updated

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <div className="text-center">
+     <h1 className="text-3xl text-accent font-bold mb-4">About Pokémon Explorer</h1>
+     <p className="text-lg text-secondary max-w-xl mx-auto mb-4">This app helps you explore Pokémon data using the PokéAPI!</p>
+      {/* Bug: Missing navigation link */}
+      <Link className="text-accent mx-4 font-bold hover:underline" href='/'>
+            Back to Home
+      </Link>{" "}
+    </div>
+  );
+}
